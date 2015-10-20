6759640e6dccde7070bff91472e14df8f07ed6c1
beanstown106 <nbenis106@gmail.com>
2015-10-20 16:31:30 +0300

    build: Clean up rom flash & add CyanPop info
    *drop the target:build fingerprint text
    *drop the patching system image unconditionally text
    *drop the verifying system image text
    *changed the verified system image text
    *add flashing system text
    
    Modified from original commit for DirtyUnicorns.
    
    Change-Id: Icc2197b000dbaa70187a8e247a9faf28dbf59c4d

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1324,9 +1324,8 @@ class BlockDifference(object):
   def WriteScript(self, script, output_zip, progress=None):
     if not self.src:
       # write the output unconditionally
-      script.Print("Patching %s image unconditionally..." % (self.partition,))
-    else:
-      script.Print("Patching %s image after verification." % (self.partition,))
+      script.Print(" ")
+      script.Print("Flashing CyanPop System files...")
 
     if progress:
       script.ShowProgress(progress, 0)
@@ -1372,7 +1371,7 @@ class BlockDifference(object):
 
   def _WritePostInstallVerifyScript(self, script):
     partition = self.partition
-    script.Print('Verifying the updated %s image...' % (partition,))
+    #script.Print('Verifying the updated %s image...' % (partition,))
     # Unlike pre-install verification, clobbered_blocks should not be ignored.
     ranges = self.tgt.care_map
     ranges_str = ranges.to_string_raw()
@@ -1387,14 +1386,16 @@ class BlockDifference(object):
       script.AppendExtra('if range_sha1("%s", "%s") == "%s" then' % (
                          self.device, ranges_str,
                          self._HashZeroBlocks(self.tgt.extended.size())))
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Verified CyanPop System files...")
       script.AppendExtra(
           'else\n'
           '  abort("%s partition has unexpected non-zero contents after OTA '
           'update");\n'
           'endif;' % (partition,))
     else:
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Verified CyanPop System files...")
 
     script.AppendExtra(
         'else\n'
